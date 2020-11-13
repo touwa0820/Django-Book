69f100a29f047f2f8334586651bab30166b6bf78
“touwa0820” <touwa100@gmail.com>
2020-11-14 00:09:55 +0900

    homepage

--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -5,34 +5,32 @@ import csv
 import requests
 
 #初期設定
-KEYID = 'da4e1585e099dac608d681acf6b4e042'
+KEYID = "da4e1585e099dac608d681acf6b4e042"
 HIT_PER_PAGE = 100
 PREF = "PREF41"
-area_code = "AREA140"
 areacode_l = "AREAL5152"
 areacode_m = "AREAM5439"
 areacode_s = "AREAS5437"
 name = ""
 name_kana = ""
 
-PARAMS = {"keyid":KEYID,"hit_per_page":HIT_PER_PAGE,"pref":PREF,"area_code":area_code,"areacode_l":areacode_l,"areacode_m":areacode_m,"areacode_s":areacode_s,"name":name,"name_kana":name_kana}
+PARAMS = {"keyid":KEYID,"hit_per_page":HIT_PER_PAGE,"pref":PREF,"areacode_l":areacode_l,"areacode_m":areacode_m,"areacode_s":areacode_s,"name":name,"name_kana":name_kana}
 
-def write_date_to_csv(parms):
-    foodlist = ["名称","住所"]
+def write_data_to_csv(params):
+    foodlists = []
     json_res = requests.get("https://api.gnavi.co.jp/RestSearchAPI/v3/",params=PARAMS).text
-    respons = json.loads(json_res)
-    if "error" in respons:
+    response = json.loads(json_res)
+    if "error" in response:
         return print("エラーが発生しました！")
-    for foodlist in respons["food"]:
-        foodlist_name = foodlist["name"]
-        foodlist.append(foodlist_name)
+    for foodlist in response["rest"]:
+        foodlist_name = [foodlist["name"]]
+        foodlists.append(foodlist_name)
     with open("food_list.csv","w") as f:
         writer = csv.writer(f)
-        writer.writerow(foodlist)
+        writer.writerows(foodlist)
     return print(foodlist)
 
-write_date_to_csv(PARAMS)
-
+write_data_to_csv(PARAMS)
 
 
 # Create your views here.
