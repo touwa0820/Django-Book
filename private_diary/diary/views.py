409d73015ade5a646cebab81acb42be9499c2539
“touwa0820” <touwa100@gmail.com>
2020-11-17 00:44:11 +0900

    homepage

--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -4,6 +4,7 @@ import json
 import csv
 import requests
 
+
         #初期設定
 
 # Create your views here.
@@ -17,31 +18,42 @@ class TableView(generic.TemplateView):
 
     def get(self,request,*args,**kwatgs,):
         KEYID = "da4e1585e099dac608d681acf6b4e042"
-        HIT_PER_PAGE = 100
         PREF = "PREF41"
         areacode_l = "AREAL5152"
-        areacode_m = "AREAM5439"
-        areacode_s = "AREAS5437"
-        name = ""
-        name_kana = ""
         PARAMS = {
             "keyid": KEYID,
-            # "hit_per_page": HIT_PER_PAGE,
             "pref": PREF,
             "areacode_l": areacode_l,
-            # "areacode_m": areacode_m,
-            # "areacode_s": areacode_s,
-            # "name": name,
-            # "name_kana": name_kana
         }
         json_res = requests.get("https://api.gnavi.co.jp/RestSearchAPI/v3/",params=PARAMS).text
         response = json.loads(json_res)
-        print(response)
         if "error" in response:
             return print("エラーが発生しました！")
-        # def get(self,request):  
         if "category" in request.GET:
             param_value = request.GET.get("category")
-            
+            if param_value == "和食":
+                category_l = "RSFST01000"
+                PARAMS = {
+                    "keyid": KEYID,
+                    "pref": PREF,
+                    "areacode_l": areacode_l,
+                    "category_l": category_l,
+                }
+                json_res = requests.get("https://api.gnavi.co.jp/RestSearchAPI/v3/",params=PARAMS).text
+                response = json.loads(json_res)
+                count = len(response['rest'])
+                for count in range(count):
+                    print(response['rest'][count]['name'])
+                
+            elif param_value == "洋食":
+                print("洋食")
+            elif param_value == "中華":
+                print("中華")
+            elif param_value == "喫茶店・カフェ":
+                print("喫茶店・カフェ")
+            elif param_value == "ファミレス":
+                print("ファミレス")
+            elif param_value == "居酒屋":
+                print("居酒屋")
             
         return render(request,"index2.html")
